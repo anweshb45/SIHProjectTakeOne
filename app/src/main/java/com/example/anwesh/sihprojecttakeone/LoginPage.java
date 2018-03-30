8988d657663ab22e07880167bc84f0109456a155
anwesh45 <anweshb45@gmail.com>
2018-03-30 10:49:55 +0530

    Added Splash on 30th

--- app/src/main/java/com/example/anwesh/sihprojecttakeone/LoginPage.java
+++ app/src/main/java/com/example/anwesh/sihprojecttakeone/LoginPage.java
@@ -2,6 +2,16 @@ package com.example.anwesh.sihprojecttakeone;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONObject;
 
 public class LoginPage extends AppCompatActivity {
 
@@ -9,5 +19,41 @@ public class LoginPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+
+        /*Button login_btn = findViewById(R.id.login_btn);
+        login_btn.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View v) {
+                //get login credentials
+                String url = "http://my-json-feed";
+
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                        (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                //mTextView.setText("Response: " + response.toString());
+                                Toast.makeText(getApplicationContext(),"Successful",Toast.LENGTH_SHORT).show();
+                            }
+                        }, new Response.ErrorListener() {
+
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                // TODO: Handle error
+
+                            }
+                        });
+
+// Access the RequestQueue through your singleton class.
+                MySingleton.getInstance(getApplicationContext()).addToRequestQueue(jsonObjectRequest);
+            }
+        });*/
+
     }
+
+
+
+
 }
