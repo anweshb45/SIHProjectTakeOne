d99b4730585371d8e9a7b980c11c82588e4fa3f2
anwesh45 <anweshb45@gmail.com>
2018-03-31 07:51:53 +0530

    Added Panchyat Finance

---
+++ app/src/main/java/com/example/anwesh/sihprojecttakeone/Finance.java
@@ -0,0 +1,194 @@
+package com.example.anwesh.sihprojecttakeone;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.util.Log;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Finance extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    String year;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_finance);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.finance, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    public void getAllocations(View v) {
+        EditText year_inp = findViewById(R.id.year_inp);
+        year = year_inp.getText().toString();
+
+        String allocationURL = "http://192.168.43.194:8086/get_allocation/" + year;
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, allocationURL, null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+
+                JSONObject jsonObject;
+                Log.d("Finance" , response.toString());
+                for(int i= 0; i<response.length() ; i++) {
+
+                    TextView textView;
+                    switch(i) {
+                        case 0 :
+                            textView = findViewById(R.id.central_allocation);
+
+                            try {
+                                jsonObject = response.getJSONObject(i);
+                                textView.setText("Year: " +year + "\nAmount: " + jsonObject.getString("amount") + "\nModal Objective: " + jsonObject.getString("modal_objective") + "\nTitle: " + jsonObject.getString("title") + "\nTimestamp: " + jsonObject.getString("timestamp"));
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                            break;
+                        case 1 : textView = findViewById(R.id.state_allocation);
+
+                            try {
+                                jsonObject = response.getJSONObject(i);
+                                textView.setText("Year: " +year + "\nAmount: " + jsonObject.getString("amount") + "\nModal Objective: " + jsonObject.getString("modal_objective") + "\nTitle: " + jsonObject.getString("title") + "\nTimestamp: " + jsonObject.getString("timestamp"));
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                            break;
+                        case 2 : textView = findViewById(R.id.scheme_allocation);
+
+                            try {
+                                jsonObject = response.getJSONObject(i);
+                                textView.setText("Year: " +year + "\nAmount: " + jsonObject.getString("amount") + "\nModal Objective: " + jsonObject.getString("modal_objective") + "\nTitle: " + jsonObject.getString("title") + "\nTimestamp: " + jsonObject.getString("timestamp"));
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                            break;
+                        case 3: textView = findViewById(R.id.misc_grant);
+
+                            try {
+                                jsonObject = response.getJSONObject(i);
+                                textView.setText("Year: " +year + "\nAmount: " + jsonObject.getString("amount") + "\nModal Objective: " + jsonObject.getString("modal_objective") + "\nTitle: " + jsonObject.getString("title") + "\nTimestamp: " + jsonObject.getString("timestamp"));
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                            break;
+
+                    }
+
+                }
+
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("Finance" , "Error: " + error.getMessage());
+            }
+        }) {
+
+        };
+
+        queue.add(jsonArrayRequest);
+    }
+}
